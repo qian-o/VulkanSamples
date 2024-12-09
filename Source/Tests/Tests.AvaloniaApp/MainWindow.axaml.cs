6dfcffcd2f712174e61125224a45f3e97cf427fd
王先生 <1324771795@qq.com>
2024-12-09 21:38:16 +0800

    添加 Avalonia 应用及相关配置
    在 `Directory.Packages.props` 文件中，添加了以下包的版本信息：
    - Avalonia 11.2.2
    - Avalonia.Desktop 11.2.2
    - Avalonia.Diagnostics 11.2.2
    - Avalonia.Fonts.Inter 11.2.2
    - Avalonia.Themes.Fluent 11.2.2
    
    更新了 `GLTF.ps.hlsl.spv` 和 `RayQuery.ps.hlsl.spv` 文件的二进制内容。
    
    在 `VulkanAbstractionLayer.sln` 解决方案文件中，添加了一个新的项目 `Tests.AvaloniaApp`，并更新了相应的解决方案配置平台。
    
    在 `App.axaml` 文件中，添加了一个新的 Avalonia 应用程序的 XAML 定义，包含 `FluentTheme` 主题。
    
    在 `App.axaml.cs` 文件中，添加了 `App` 类的定义，包含应用程序的初始化和框架初始化完成后的处理逻辑。
    
    在 `app.manifest` 文件中，添加了一个新的应用程序清单文件，定义了应用程序的兼容性信息。
    
    在 `MainWindow.axaml` 文件中，添加了一个新的 Avalonia 窗口的 XAML 定义，包含一个 `Grid` 控件。
    
    在 `MainWindow.axaml.cs` 文件中，添加了 `MainWindow` 类的定义，包含窗口的初始化逻辑。
    
    在 `Program.cs` 文件中，添加了 `Program` 类的定义，包含应用程序的入口点和 Avalonia 应用程序的构建逻辑。
    
    在 `Tests.AvaloniaApp.csproj` 文件中，添加了一个新的项目文件，定义了项目的属性、包引用和项目引用。

---
+++ Source/Tests/Tests.AvaloniaApp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tests.AvaloniaApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/Tests/Tests.AvaloniaApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+
+namespace Tests.AvaloniaApp;
+
+internal class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+                         .UsePlatformDetect()
+                         .WithInterFont()
+                         .LogToTrace();
+    }
+}
